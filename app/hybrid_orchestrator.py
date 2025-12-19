0edf9efe6c6bcc03d861732609d8cdd222be64f9
Nayeemuddinga <gulam.ahmed786@outlook.com>
2025-12-20 03:52:17 +0530

    Add function to fuse scores from two result sets

--- app/hybrid_orchestrator.py
+++ app/hybrid_orchestrator.py
@@ -1,1 +1,8 @@
+def fuse_scores(vector_results, keyword_results):
+    scores = {}
+    for r in vector_results:
+        scores[r["doc_id"]] = scores.get(r["doc_id"], 0) + 0.6
+    for r in keyword_results:
+        scores[r["doc_id"]] = scores.get(r["doc_id"], 0) + 0.4
+    return sorted(scores.items(), key=lambda x: x[1], reverse=True)
 
